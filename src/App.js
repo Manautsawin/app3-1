19d4b8b7e41b49ca7eb902411a6656da75220082
Manautsawin <265345221020-st@rmutsb.ac.th>
2023-05-01 12:18:22 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import Button from './func-event'
+//import Calculator from './func-calcutor'
+//import EventObject from './event-data'
+import Table from './table'
+function App() {
+  return (
+    //<Button />,
+    //<Calculator />
+    //<EventObject />
+    <Table />
+  )
+}
+
+export default App;
